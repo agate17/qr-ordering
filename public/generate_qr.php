0f16d586153b1036390d7b53f1a97bdfad1da4c7
goldwolf20 <agate.katane@gmail.com>
2025-08-07 14:45:38 +0300

    added both url (commented but for understanfing and easier finding)

--- public/generate_qr.php
+++ public/generate_qr.php
@@ -9,6 +9,11 @@ if ($table < 1 || $table > get_table_count()) {
     exit;
 }
 $url = "http://192.168.8.135/qr-ordering/qr-ordering/public/menu.php?table=$table";
+
+// home url - 192.168.8.135
+// bouling url - 192.168.8.7 (vismaz iepriekš bij ta)
+
+
 // Try to use endroid/qr-code if available
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
