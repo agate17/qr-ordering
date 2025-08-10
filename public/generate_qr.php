2df1506eac1eb51a5ef78de8d09b483da993cc0e
goldwolf20 <agate.katane@gmail.com>
2025-08-10 21:03:48 +0300

    url change

--- public/generate_qr.php
+++ public/generate_qr.php
@@ -8,7 +8,7 @@ if ($table < 1 || $table > get_table_count()) {
     echo 'Invalid table number.';
     exit;
 }
-$url = "http://192.168.8.23/qr-ordering/qr-ordering/public/menu.php?table=$table";
+$url = "http://192.168.8.23/qr-ordering/public/menu.php?table=$table";
 
 // home url - 192.168.8.135
 // bouling url - 192.168.8.7 (vismaz iepriekš bij ta)
